9de8d88cee4522c4faea032e04199a942855df8e
bitSheriff <bitSheriff@protonmail.com>
2022-08-24 16:33:10 +0200

    prep for commits

--- src/gitter.rs
+++ src/gitter.rs
@@ -1,20 +1,29 @@
-#![crate_name = "gitter"]
+
+use lipsum::lipsum;
+
+const LOREM_WORDS: u8 = 8;
+
+
 #[derive(Debug)]
 pub struct Gitter {
     path: String,
-    fake_file: String
+    fake_file: String,
+    num_commits: u32
 }
 
 
 impl Gitter {
     pub fn new(path: String, file: String) -> Gitter {
         Self {  path: path,
-                fake_file: file
+                fake_file: file,
+                num_commits: 0
         }
     }
-
-    pub fn commit(msg: String) {
-        
+    
+    /// Commit
+    /// command should look like "git commit --date='year-month-day hour:minutes:seconds' -m "message""
+    pub fn commit(&mut self, date_string: String) {
+        let msg = lipsum::lipsum_words(LOREM_WORDS.into());    
     }
     
     pub fn init_file() {
@@ -26,4 +35,5 @@ impl Gitter {
     pub fn check_repository() {
 
     }
+
 }
--- src/systemer.rs
+++ src/systemer.rs
@@ -1,7 +1,19 @@
+use std::process::Command;
 
+pub fn system_call(cmd: String, args: String) -> String {
 
-pub fn system_call(cmd: String) -> String {
-    let output = String::new();
-
-    output
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(cmd)
+            .args(args)
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+            .arg(cmd)
+            .arg(args)
+            .output()
+            .expect("failed to execute process")
+    };
+    output.stdout
 }
