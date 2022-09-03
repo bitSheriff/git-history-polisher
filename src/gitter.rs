f101bd6f6a87fa75af6b629d03238c0f03dd251c
bitSheriff <bitSheriff@protonmail.com>
2022-09-03 10:54:20 +0200

    generate random amount of commits if wanted

--- src/gitter.rs
+++ src/gitter.rs
@@ -1,5 +1,7 @@
 
 use lipsum::lipsum;
+use rand::Rng;
+use crate::systemer;
 
 const LOREM_WORDS: u8 = 8;
 
@@ -8,26 +10,55 @@ const LOREM_WORDS: u8 = 8;
 pub struct Gitter {
     path: String,
     fake_file: String,
-    num_commits: u32
+    min_commits: u16,
+    max_commits: u16,
+    num_commits: u32,
+    num_commits_done_day: u16,
 }
 
 
 impl Gitter {
-    pub fn new(path: String, file: String) -> Gitter {
+    pub fn new(path: String, file: String, min: u16, max: u16) -> Gitter {
         Self {  path: path,
                 fake_file: file,
+                min_commits: min,
+                max_commits: max,
+                num_commits_done_day: 0,
                 num_commits: 0
         }
     }
     
+    pub fn commit_on_date(&mut self, &date_string:&std::string::String) {
+        let mut commits:u16 = 1;
+
+        if self.min_commits != self.max_commits
+        {
+            let mut rng = rand::thread_rng();
+            commits = rng.gen_range(self.min_commits .. self.max_commits);
+        }
+
+        for i in 0..commits {
+            self.commit(&date_string);
+        }
+    }
+
     /// Commit
     /// command should look like "git commit --date='year-month-day hour:minutes:seconds' -m "message""
-    pub fn commit(&mut self, date_string: String) {
-        let msg = lipsum::lipsum_words(LOREM_WORDS.into());    
+    fn commit(&mut self, &date_string: &std::string::String) {
+        let msg = lipsum::lipsum_words(LOREM_WORDS.into());
+
+        systemer::git_commit(&self.path, msg);
+
+        // increase the the internal commit counter
+        self.num_commits += 1;
     }
     
     pub fn init_file() {
 
+    }
+
+    pub fn get_num_commits(&self) -> u32 {
+        self.num_commits
     }
 
     /// Check Repository
