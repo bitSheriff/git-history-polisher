2138fd7cce0dde982dafcafc554fdade003d67fd
bitSheriff <bitSheriff@protonmail.com>
2022-09-03 10:14:26 +0200

    change file

--- src/systemer.rs
+++ src/systemer.rs
@@ -1,5 +1,8 @@
 use std::process::Command;
-
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+ 
 pub fn system_call(cmd: String, args: String) {
 }
 
@@ -24,14 +27,19 @@ pub fn git_commit(dir: String, msg: String) {
                   .status();
 }
 
-pub fn sys_create_file(name: String) {
+pub fn sys_change_file(pathString: String, name: String, text: String) {
 
-    let command = Command::new("touch")
-                  .arg(name)
-                  .status();
-}
+    let path = Path::new(&pathString);
 
-pub fn sys_change_file(name: String) {
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", path.display(), why),
+        Ok(file) => file,
+    };
+
+    match file.write_all(text.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", path.display(), why),
+        Ok(_) => println!("successfully wrote to {}", path.display()),
+    }
 
 }
 
